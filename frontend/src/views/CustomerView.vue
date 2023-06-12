735b058082b645ca3c9b7dd2c4d4e11894e46836
Philip Yong <philipyong@live.com.my>
2023-06-12 17:18:46 -0500

    Added Customer View

---
+++ frontend/src/views/CustomerView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      latest_serving_number: '0001',
+      last_issued_number: '0002'
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div
+      class="md mt-10 flex flex-col justify-center rounded-lg border-2 border-solid border-black p-6"
+    >
+      <h1 class="pb-2 text-4xl">Now Serving: {{ latest_serving_number }}</h1>
+      <h1 class="pb-4 text-4xl">Last number: {{ last_issued_number }}</h1>
+      <button class="self-center rounded-lg border-2 border-solid border-black px-4 py-2 text-3xl">
+        Take a Number
+      </button>
+    </div>
+  </div>
+</template>
